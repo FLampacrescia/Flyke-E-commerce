44151794bd8284051c0995e67fa78ba889b63ad9
FLampacrescia <flampacrescia@gmail.com>
2025-04-10 17:43:56 -0300

    Fix responsive in about page

--- src/components/About/SmallTextGroup/SmallTextGroup.jsx
+++ src/components/About/SmallTextGroup/SmallTextGroup.jsx
@@ -2,19 +2,19 @@ import "./SmallTextGroup.css"
 
 export default function SmallTextGroup() {
     return (
-        <div className="description-row-container">
-            <div className="description-left">
-                <h3 className="description-title">¿Quiénes somos?</h3>
-                <p className="description">
+        <div className="about-description-row-container">
+            <div className="about-description-left">
+                <h3 className="about-description-title">¿Quiénes somos?</h3>
+                <p className="about-description">
                     Fundada en Buenos Aires en 2024, nuestra empresa nació con un propósito
                     claro: impulsar los sueños y el rendimiento deportivo de cada persona. Nos
                     mueve la pasión por el deporte y el compromiso de brindar productos que no
                     solo mejoren el rendimiento, sino que también inspiren.
                 </p>
             </div>
-            <div className="description-right">
-                <h3 className="description-title">Nuestra visión</h3>
-                <p className="description">
+            <div className="about-description-right">
+                <h3 className="about-description-title">Nuestra visión</h3>
+                <p className="about-description">
                     Transformar al mundo a través del poder del deporte. Creemos que cada
                     persona tiene un atleta interior, y nuestra misión es apoyarlo en cada
                     paso, ya sea en la cancha, en las calles o en la vida cotidiana. En
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,5 @@
 import "./About.css";
-import SmallTextGroup from "../../components/Common/SmallTextGroup/SmallTextGroup";
+import SmallTextGroup from "../../components/About/SmallTextGroup/SmallTextGroup";
 import SocialIcons from "../../components/Common/SocialIcons/SocialIcons";
 import MainTitle from "../../components/Common/MainTitle/MainTitle";
 import AboutImg from "../../assets/about1.jpg";
@@ -11,12 +11,12 @@ export default function About() {
         <section className="about-container">
             <div className="about-text-main-container">
                 <MainTitle classAddContainer="about-title-container" classAdd="about-title" text="HACELO POSIBLE." spanText=" NOSOTROS PONEMOS LAS ZAPATILLAS."/>
-                <div className="description-column-container">
+                <div className="about-description-column-container">
                     <SmallTextGroup />
                     <SocialIcons type="social-container" type2="social-logo-container" type3="social-logo"/>
                 </div>
             </div>
-            <div className="img-container">
+            <div className="about-img-container">
                 <img className="about-img" src={AboutImg} alt="Nike About Photo" />
             </div>
         </section>
