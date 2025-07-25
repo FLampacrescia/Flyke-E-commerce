b834a7efc8a65bc19e3dcf3c764fd4a33e631fff
FLampacrescia <flampacrescia@gmail.com>
2025-07-25 17:26:24 -0300

    Implement loader in home when loading products.

--- src/components/Common/Loaders/CircleLoader/CircleLoader.jsx
+++ src/components/Common/Loaders/CircleLoader/CircleLoader.jsx
@@ -1,8 +1,8 @@
 import "./CircleLoader.css"
 
-export default function CircleLoader() {
+export default function CircleLoader({ classAdd }) {
     return (
-        <div className="circle-loader-container">
+        <div className={`circle-loader-container ${classAdd}`}>
             <div className="circle-loader"></div>
         </div>
     )
--- src/components/Home/ProductsSection/ProductList/ProductList.jsx
+++ src/components/Home/ProductsSection/ProductList/ProductList.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import config from '../../../../config/env.config';
 import ProductCard from "../ProductCard/ProductCard";
 import api from "../../../../utils/axiosInstance";
+import CircleLoader from "../../../Common/Loaders/CircleLoader/CircleLoader";
 
 export default function ProductList() {
 
     const [products, setProducts] = useState([]);
+    const [loadingProducts, setLoadingProducts] = useState(true);
 
     useEffect(() => {
         getProducts();
@@ -13,21 +15,30 @@ export default function ProductList() {
 
     async function getProducts() {
         try {
+            setLoadingProducts(true);
             const response = await api.get(`${config.API_URL}/products`);
             const products = response.data.products;
-            setProducts(products);
+            setTimeout(() => {
+                setProducts(products);
+                setLoadingProducts(false);
+            }, 1000);
         } catch (error) {
             console.error(error);
+            setLoadingProducts(false);
         }
     }
     
     return (
         <div className="product-container">
-
-            {products?.map((product) => (
-
-                <ProductCard key={product._id} product={product}/>
-            ))}
+            {loadingProducts ? (
+                <div className="product-loading-container">
+                    <CircleLoader classAdd="circle-loader-product-screen" />
+                    <p className="product-loading-message">Cargando Productos</p>
+                </div>
+            ) : (
+                products?.map((product) => (
+                    <ProductCard key={product._id} product={product} /> ))
+            )}
         </div>
     )
 }
