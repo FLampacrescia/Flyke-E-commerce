e133080ab8558a2ac7d9f2f4817e8cfd977e1bfe
FLampacrescia <flampacrescia@gmail.com>
2025-08-05 01:46:21 -0300

    Add order card component to show in orders section in MyAccount

---
+++ src/components/MyAccount/MyAccountOrdersSection/MyAccountOrderCard/MyAccountOrderCard.jsx
@@ -0,0 +1,98 @@
+import "./MyAccountOrderCard.css";
+import { useState } from "react";
+import { useTranslation } from "../../../../hooks/useTranslations";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { motion, AnimatePresence } from "framer-motion";
+import OrderSuccessUnit from "../../../Checkout/OrderSuccess/OrderSuccessUnit/OrderSuccessUnit";
+
+export default function MyAccountOrderCard({ order }) {
+    const { t } = useTranslation();
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleExpand = () => setIsExpanded(!isExpanded);
+
+    const totalItems = order.products.reduce((acc, item) => acc + item.quantity, 0);
+    const date = new Date(order.createdAt);
+
+    const time = date.toLocaleTimeString("es-AR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+    });
+
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = date.toLocaleString("es-AR", { month: "short" });
+    const capitalizedMonth = month.charAt(0).toUpperCase() + month.slice(1);
+    const year = date.getFullYear();
+
+    const formattedDate = `${time}, ${day} ${capitalizedMonth}, ${year}`;
+    const formattedAmount = `$${new Intl.NumberFormat('es-AR').format(order.total)}`;
+
+    const statusClass = {
+        pending: "order-status pending",
+        completed: "order-status completed",
+        canceled: "order-status canceled"
+    }[order.status] || "order-status";
+
+    return (
+        <div className="order-card">
+            <div className="order-card-content-container">
+                <div className="order-card-title-main-container">
+                    <div className="order-card-header-container">
+                        <div className="order-card-title-container">
+                            <p className="order-code">{`${t('myaccount_order_card_order')} #${order.orderCode}`}</p>
+                            <span className={statusClass}>
+                                {t(`order_status_${order.status}`)}
+                            </span>
+                        </div>
+                        <button
+                            className={`chevron-toggle ${isExpanded ? "expanded" : ""}`}
+                            onClick={toggleExpand}
+                            aria-label={isExpanded ? t("hide_details") : t("show_details")}
+                        >
+                            <FontAwesomeIcon icon={faChevronDown} />
+                        </button>
+                    </div>
+                    <p className="order-details-summary">
+                        {totalItems} {t("myaccount_order_card_products")} | {formattedDate}
+                    </p>
+                </div>
+                <p className="order-total">{formattedAmount}</p>
+                <AnimatePresence initial={false}>
+                    {isExpanded && (
+                        <motion.div
+                            className="order-card-expanded-content"
+                            initial={{ height: 0, opacity: 0 }}
+                            animate={{ height: "auto", opacity: 1 }}
+                            exit={{ height: 0, opacity: 0 }}
+                            transition={{ duration: 0.3 }}
+                        >
+                            <div className="order-card-expanded-shipping-section">
+                                <div className="order-card-expanded-shipping-title-container">
+                                    <p className="order-card-expanded-shipping-title">Tipo de Envío:</p>
+                                    <p className="order-card-expanded-shipping-title">Dirección de Envío:</p>
+                                </div>
+                                <div className="order-card-expanded-shipping-info-container">
+                                    <p className="order-card-expanded-shipping-info">{order.shipping}</p>
+                                    <p className="order-card-expanded-shipping-info">
+                                        {order.shipping === "pickup"
+                                            ? `${order.store.name} - ${order.store.address}`
+                                            : order.shippingAddress
+                                                ? `${order.shippingAddress.street}, ${order.shippingAddress.neighborhood}, ${order.shippingAddress.province}, CP ${order.shippingAddress.zipCode}`
+                                                : "-"}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="order-card-expanded-cart-section">
+                                {order.products.map((product) => (
+                                    <OrderSuccessUnit key={product._id} product={product} />
+                                ))}
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </div>
+        </div>
+    );
+}
--- src/components/MyAccount/MyAccountOrdersSection/MyAccountOrdersSection.jsx
+++ src/components/MyAccount/MyAccountOrdersSection/MyAccountOrdersSection.jsx
@@ -1,9 +1,69 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useTranslation } from "../../../hooks/useTranslations";
+import api from "../../../utils/axiosInstance";
+import config from "../../../config/env.config";
+import { useUser } from "../../../context/UserContext";
+import { motion, AnimatePresence } from "framer-motion";
+import CircleLoader from "../../Common/Loaders/CircleLoader/CircleLoader";
+import MyAccountOrderCard from "./MyAccountOrderCard/MyAccountOrderCard";
+import "./MyAccountOrdersSection.css"
 
 
 export default function MyAccountOrdersSection() {
+
+    const { user } = useUser();
+    const [orders, setOrders] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const { t } = useTranslation();
+
+    useEffect(() => {
+        if (user?._id) {
+            getOrders();
+        }
+    }, [user]);
+
+    async function getOrders() {
+        try {
+            setLoading(true);
+            const response = await api.get(`${config.API_URL}/users/${user._id}/orders`);
+
+            setTimeout(() => {
+                setOrders(response.data);
+                setLoading(false);
+            }, 700);
+        } catch (error) {
+            console.error(error);
+            toast.error(t('myaccount_order_section_load_error'))
+            setLoading(false);
+        }
+    }
+
     return (
-        <div>
-            <p>asdasdas</p>
-        </div>
+        <AnimatePresence mode="wait">
+            <motion.div 
+                className="my-account-orders-main-container"
+                key="orders"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5, ease: [0.6, 0.05, 0.55, 1] }}>
+                    {loading ? (
+                        <div className="wishlist-loading-container">
+                            <CircleLoader classAdd="circle-loader-local-use" />
+                        </div>
+                    ) : orders && orders.length > 0 ? (
+                        orders.map((order) => (
+                                <MyAccountOrderCard
+                                    key={order._id}
+                                    order={order}
+                                />
+                            ))
+                    ) : (
+                        <p className="wishlist-empty-message text-center">{t('myaccount_order_section_empty_message')}</p>
+                    )}
+            </motion.div>
+        </AnimatePresence>
     )
 }
